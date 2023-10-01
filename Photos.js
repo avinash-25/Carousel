f632723c2e9ff389bea5478266e9d9d1e8aa242b
avinash-25 <avinashranjan268@gmail.com>
2023-10-01 16:38:08 +0530

    final commit

--- Photos.js
+++ Photos.js
@@ -5,7 +5,11 @@ function Photos() {
   const images = [
     './SlidesPic/pic1.jpg',
     './SlidesPic/pic2.jpg',
-    './SlidesPic/pic3.jpg'
+    './SlidesPic/pic3.jpg',
+    './SlidesPic/pic4.jpg',
+    './SlidesPic/pic5.jpg',
+    './SlidesPic/pic6.jpg',
+    './SlidesPic/pic7.jpg'
      //Add more images with correct path  
   ];
 
